83cce7d84f83373f542b6aa07af8bb1d4bc051f3
Alex Carqueja <up201705049@fe.up.pt>
2025-02-22 22:27:48 +0000

    optimal solution for image rotation

---
+++ 48.rotate-image.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=48 lang=python3
+#
+# [48] Rotate Image
+#
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for row_index in range(0, len(matrix)):
+            for col_index in range (len(matrix) - 1, -1, -1):
+                row = matrix[row_index]
+                value = row[len(row) - 1]
+                del row[len(row) - 1]
+                matrix[col_index] = [value] + matrix[col_index]
+
+
+
+        
+# @lc code=end
+
