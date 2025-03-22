fed993bf6e000de466545c2c3930d90dcc87d430
Alex Carqueja <up201705049@fe.up.pt>
2025-03-22 16:11:16 +0000

    palindrome exponential solution

--- 131.palindrome-partitioning.py
+++ 131.palindrome-partitioning.py
@@ -5,79 +5,28 @@
 #
 from typing import List
 # @lc code=start
+
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        palindromes = [list(s)]
+        
+        def is_palindrome(string : str) -> bool:
+            return string == string[::-1]
+         
+        def find_palindromes(palindrome: List[str], s : str) -> List[List[str]]:
+            found_palindromes = []
 
-        def create_new_full_palindromes(palindrome: str, prefix_length: str, suffix_length : int, existing_palindromes: List[List[str]]) ->  List[List[str]]:
-            new_full_palindromes = []
-            for existing_palindrome in existing_palindromes:
-                
-                prefix = []
-                missing_prefix_characters = prefix_length
-                while(missing_prefix_characters > 0):
-                    try:
-                        prefix_character = existing_palindrome[len(prefix)]
-                    except IndexError:
-                        missing_prefix_characters = -1
-                        break
-                    prefix.append(prefix_character)
-                    missing_prefix_characters -= len(prefix_character)
-                if missing_prefix_characters < 0:
-                    continue
+            if not s:
+                found_palindromes.append(palindrome)
 
-                suffix = []
-                missing_suffix_characters = suffix_length
-                while(missing_suffix_characters > 0):
-                    try:
-                        suffix_character = existing_palindrome[len(prefix) + len(palindrome) + len(suffix)]
-                    except IndexError:
-                        missing_suffix_characters = -1
-                        break
-
-                    suffix.append(suffix_character)
-                    missing_suffix_characters -= len(suffix_character)
-                if missing_suffix_characters < 0:
-                    continue
-
-                new_palindrome = prefix + [palindrome] + suffix
-                if new_palindrome in new_full_palindromes:
-                    continue
-                
-                new_full_palindromes.append(new_palindrome)
+            for palindrome_size in range(1, len(s)+1):
+                palindrome_candidate = s[:palindrome_size]
+                if is_palindrome(palindrome_candidate):
+                    new_palindrome = palindrome + [palindrome_candidate]
+                    found_palindromes += find_palindromes(new_palindrome, s[palindrome_size:])
             
-            return new_full_palindromes
+            return found_palindromes
 
-
-        for palindrome_end_offset in range(1,len(s)):
-            for substring_start_index in range(len(s)-palindrome_end_offset):
-                substring_end_index = substring_start_index + palindrome_end_offset
-                
-                substring = s[substring_start_index:substring_end_index+1]
-                reversed_substring = substring[::-1]
-                if substring == reversed_substring:
-                    suffix_length = len(s) - (substring_end_index + 1)
-                    new_palindromes = create_new_full_palindromes(substring, substring_start_index, suffix_length, palindromes)
-                    palindromes += new_palindromes
-
-        return palindromes
-
-    def partition_incomplete(self, s: str) -> List[List[str]]:
-        palindromes = [list(s)]
-
-        for palindrome_end_offset in range(1,len(s)):
-            for substring_start_index in range(len(s)-palindrome_end_offset):
-                substring_end_index = substring_start_index + palindrome_end_offset
-                
-                substring = s[substring_start_index:substring_end_index+1]
-                reversed_substring = substring[::-1]
-                if substring == reversed_substring:
-                    palindrome_prefix = list(s[:substring_start_index])
-                    palindrome_suffix = list(s[substring_end_index+1:])
-                    palindrome = palindrome_prefix + [substring] + palindrome_suffix
-                    palindromes.append(palindrome)
-
-        return palindromes
+        return find_palindromes([], s)
         
 # @lc code=end
 
--- tests/test_palindromes-partitioning.py
+++ tests/test_palindromes-partitioning.py
@@ -9,11 +9,16 @@ spec = importlib.util.spec_from_file_location(
 leetcode_file = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(leetcode_file)
 solution = leetcode_file.Solution()
+# Import end
+
+from functools import reduce
 
 def test_basic_answer():
     result = solution.partition("aab")
     assert result == [["a","a","b"],["aa","b"]]
 
 def test_aaaa_answer():
+    CORRECT_ANSWER = [['a', 'a', 'a', 'a'], ['aa', 'a', 'a'], ['a', 'aa', 'a'], ['a', 'a', 'aa'], ['aa', 'aa'], ['aaa', 'a'], ['a', 'aaa'], ['aaaa']]
+    
     result = solution.partition("aaaa")
-    assert result == [['a', 'a', 'a', 'a'], ['aa', 'a', 'a'], ['a', 'aa', 'a'], ['a', 'a', 'aa'], ['aa', 'aa'], ['aaa', 'a'], ['a', 'aaa'], ['aaaa']]
+    assert reduce(lambda acc, i: (i in CORRECT_ANSWER) & acc, result, True) and len(result) == len(CORRECT_ANSWER)
