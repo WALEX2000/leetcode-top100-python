da6238b1151100fbe257b4e78046241156f06dbe
Alex Carqueja <up201705049@fe.up.pt>
2025-04-10 01:43:00 +0100

    slow solution to unique paths working

---
+++ 62.unique-paths.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=62 lang=python3
+#
+# [62] Unique Paths
+#
+
+# @lc code=start
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        num_down = m - 1
+        num_right = n - 1
+        
+        def construct_moves(right_moves: int, down_moves: int):
+            constructed_moves = 0
+            if right_moves == 0 or down_moves == 0:
+                return 1
+            
+            constructed_moves += construct_moves(right_moves-1, down_moves)
+            constructed_moves += construct_moves(right_moves, down_moves-1)
+
+            return constructed_moves
+        
+        return construct_moves(num_right, num_down)
+        
+# @lc code=end
